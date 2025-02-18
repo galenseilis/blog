5149c91253192b03b16cfd9436b33b9294969dc7
galenseilis <galen.seilis@seilis.ca>
2025-02-17 22:25:32 -0800

    rust notebook

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sudo apt-get install pypy3
 snap install zig --classic --beta
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install --locked evcxr_jupyter
+evcxr_jupyter --install
