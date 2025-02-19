f388028b3d36fb67a2b07bbb3ebd2dca0217179e
galenseilis <galen.seilis@seilis.ca>
2025-02-19 06:49:39 -0800

    fix setup; added adjacency graph posts

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ snap install zig --classic --beta
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --locked evcxr_jupyter
 evcxr_jupyter --install
+
+pip install libgraphviz-dev
