7dfca67f34d961e002ca1ad160ec990dcacaf021
galenseilis <galen.seilis@seilis.ca>
2026-06-16 06:40:07 -0700

    comments for data loading example"

--- blog/posts/0001-python-modules-can-pass-data-too/foo.py
+++ blog/posts/0001-python-modules-can-pass-data-too/foo.py
@@ -1,18 +1,22 @@
 """Load the foo data."""
 
+# INFO: IMPORTS
 import json
 import pathlib
 
-# Open the file safely using a context manager
+# INFO: Configuration file IO
 FILE_PATH = pathlib.Path("data.json")
 MODE = "r"
 ENCODING = "utf-8"
 
+# INFO: Configuration data validation.
+EXPECTED_FIELD = "datasetName"
+
+# INFO: Open the file safely using a context manager
 with FILE_PATH.open(MODE, encoding=ENCODING) as file:
     data = json.load(file)
 
 # NOTE: We can put data validation right after loading for fail-fast data validation.
-EXPECTED_FIELD = "datasetName"
 if not data.get(EXPECTED_FIELD):
     msg = f"dataset must have {EXPECTED_FIELD}?"
     raise KeyError(msg)
