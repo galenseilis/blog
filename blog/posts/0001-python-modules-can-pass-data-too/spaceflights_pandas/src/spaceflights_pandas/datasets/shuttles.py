933500b03b04a5d2aa963e9f3f4d24de91126a69
galenseilis <galen.seilis@seilis.ca>
2026-06-17 06:27:17 -0700

    Setup shuttles dataset for post 1

---
+++ blog/posts/0001-python-modules-can-pass-data-too/spaceflights_pandas/src/spaceflights_pandas/datasets/shuttles.py
@@ -0,0 +1,15 @@
+"""Shuttles dataset.
+"""
+
+# INFO: Load dependencies
+import pandas as pd
+
+# INFO: Setup dataset configuration
+URL  = "https://github.com/kedro-org/kedro-starters/raw/refs/heads/main/spaceflights-pandas/%7B%7B%20cookiecutter.repo_name%20%7D%7D/data/01_raw/shuttles.xlsx"
+
+# INFO: Load data
+data = pd.read_excel(URL)
+
+if not isinstance(data, pd.DataFrame):
+    msg = f"Expected {data=} to be of type {pd.DataFrame}, but got {type(data)=}"
+    raise ValueError(msg)
