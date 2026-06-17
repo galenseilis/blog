a2324041da1ca811733080da4b5b0c601403c6e6
galenseilis <galen.seilis@seilis.ca>
2026-06-17 07:27:19 -0700

    Working towards visualizing the spaceflights pandas project

--- blog/build.bash
+++ blog/build.bash
@@ -13,6 +13,10 @@ uvx ruff check \
 uvx typos posts
 
 # INFO: $0001
+echo "$(date): 0001: Creating tree diagram of paths in simple import module example..."
+tree ./posts/0001-python-modules-can-pass-data-too/simple_example/ > ./posts/0001-python-modules-can-pass-data-too/analysis_simple_example/tree.txt
+echo "done"
+
 echo "$(date): 0001: Building dependency graph of simple module imports..."
 uvx pydeps \
   ./posts/0001-python-modules-can-pass-data-too/simple_example/bar.py \
@@ -21,10 +25,15 @@ uvx pydeps \
   dot -Tpng -Gdpi=300 -o ./posts/0001-python-modules-can-pass-data-too/analysis_simple_example/dependency_graph.png
 echo "done"
 
-echo "$(date): 0001: Creating tree diagram of paths in simple import module example..."
-tree ./posts/0001-python-modules-can-pass-data-too/simple_example/ > ./posts/0001-python-modules-can-pass-data-too/analysis_simple_example/tree.txt
-echo "done"
-
 echo "$(date): 0001: Creating tree diagram of paths in spaceflights pandas example..."
 tree ./posts/0001-python-modules-can-pass-data-too/spaceflights_pandas/ > ./posts/0001-python-modules-can-pass-data-too/analysis_spaceflights_pandas/tree.txt
+echo "done"
+
+
+echo "$(date): 0001: Building dependency graph of de-Kedro-ed spaceflights example module imports..."
+uvx pydeps \
+  ./posts/0001-python-modules-can-pass-data-too/spaceflights_pandas/ \
+  --no-output \
+  --show-dot |
+  dot -Tpng -Gdpi=300 -o ./posts/0001-python-modules-can-pass-data-too/analysis_spaceflights_pandas/dependency_graph.png
 echo "done"
